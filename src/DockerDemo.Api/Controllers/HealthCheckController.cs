ee1e67bcdbb91d6efb891be78b514a066ad24cfb
unknown <claudio_tag@BRPOLOWN016268.stone.local>
2019-10-11 12:56:41 -0300

    Adicionado o swagger e api que retorna a data e hora

---
+++ src/DockerDemo.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        /// <summary>
+        /// Returns the current date and time
+        /// </summary>
+        /// <returns>String Value</returns>
+        
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
